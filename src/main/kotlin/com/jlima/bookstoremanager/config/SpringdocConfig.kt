7da22c8e37bb747d6c248667348a9f96919a3962
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-01 11:03:39 -0300

    build(springdoc): add springdoc and configurations

---
+++ src/main/kotlin/com/jlima/bookstoremanager/config/SpringdocConfig.kt
@@ -0,0 +1,17 @@
+package com.jlima.bookstoremanager.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+
+
+@OpenAPIDefinition(
+    info = Info(
+        title = "Bookstore Manager API",
+        description = "Bookstore Manager Project",
+        version = "1.0.0",
+        contact = Contact(name = "Jonathan Lima", url = "https://github.com/jlimadev", email = "jlima.dev@gmail.com")
+    ),
+)
+class SpringdocConfig {
+}
