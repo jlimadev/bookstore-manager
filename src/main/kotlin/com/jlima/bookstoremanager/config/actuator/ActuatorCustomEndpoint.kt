9e32242c2d632c8dcba3c3391b8402a2239026b7
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-02 17:14:37 -0300

    refactor(project): update actuator data

--- src/main/kotlin/com/jlima/bookstoremanager/config/actuator/ActuatorCustomEndpoint.kt
+++ src/main/kotlin/com/jlima/bookstoremanager/config/actuator/ActuatorCustomEndpoint.kt
@@ -4,23 +4,20 @@ import org.springframework.boot.actuate.endpoint.annotation.ReadOperation
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse
 import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension
 import org.springframework.boot.actuate.info.InfoEndpoint
+import org.springframework.boot.info.BuildProperties
 import org.springframework.stereotype.Component
 
 @Component
 @EndpointWebExtension(endpoint = InfoEndpoint::class)
 class ActuatorCustomEndpoint(
-    private val infoEndpoint: InfoEndpoint
+    private val infoEndpoint: InfoEndpoint,
+    private val buildProperties: BuildProperties
 ) {
     @ReadOperation
     fun info(): WebEndpointResponse<Map<String, Any>> {
         val info: Map<String, Any> = this.infoEndpoint.info()
-        val status = getStatus(info)
         val customInfo = HashMap<String, Any>(info)
-        customInfo["customInfo"] = "customValue"
-        return WebEndpointResponse(customInfo, status)
-    }
-
-    private fun getStatus(status: Map<String, Any>): Int {
-        return 200
+        customInfo["actuatorConfig"] = "custom configuration from actuator config file"
+        return WebEndpointResponse(customInfo, 200)
     }
 }
