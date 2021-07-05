c9c7c6c69bf325c6568d1cd2a0cbec18b4130e9a
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-05 15:47:24 -0300

    feature(domain.author): create JPA Entity

---
+++ src/main/kotlin/com/jlima/bookstoremanager/providers/entity/AuthorEntity.kt
@@ -0,0 +1,28 @@
+package com.jlima.bookstoremanager.providers.entity
+
+import java.util.Date
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
+
+@Entity
+@Table(schema = "domain", name = "author")
+data class AuthorEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    val id: UUID? = null,
+
+    @Column(name = "name", length = 255)
+    val name: String,
+
+    @Column(name = "birth_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    val birthDate: Date
+)
