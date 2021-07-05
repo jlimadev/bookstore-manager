7ada51ec31e427b624f22b843b384fc1723609d7
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-05 15:41:01 -0300

    feature(database): create tables migrations

--- src/main/resources/db/changelog/v1.0.0/db.003-create-domain.author.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset db.create-domain.author:1
-
-CREATE TABLE IF NOT EXISTS domain.author(
-    id          uuid DEFAULT uuid_generate_v4(),
-    birth_date  date,
-    primary key (id)
-);
