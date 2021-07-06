85bf0cb5dc44758d5a3e08abee12feefdc3661ab
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-06 09:56:11 -0300

    db(auditable): add auditable columns on database migrations

--- src/main/resources/db/changelog/v1.0.0/db.004-add-auditable-columns.sql
+++ src/main/resources/db/changelog/v1.0.0/db.004-add-auditable-columns.sql
@@ -3,20 +3,20 @@
 --changeset db.004-add-auditable-columns:1
 ALTER TABLE domain.author
     ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    ADD COLUMN updated_at TIMESTAMPTZ DEFAULT now(),
-    ADD COLUMN deleted_at TIMESTAMPTZ DEFAULT now();
+    ADD COLUMN updated_at TIMESTAMPTZ,
+    ADD COLUMN deleted_at TIMESTAMPTZ;
 
 ALTER TABLE domain.publisher
     ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    ADD COLUMN updated_at TIMESTAMPTZ DEFAULT now(),
-    ADD COLUMN deleted_at TIMESTAMPTZ DEFAULT now();
+    ADD COLUMN updated_at TIMESTAMPTZ,
+    ADD COLUMN deleted_at TIMESTAMPTZ;
 
 ALTER TABLE domain."user"
     ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    ADD COLUMN updated_at TIMESTAMPTZ DEFAULT now(),
-    ADD COLUMN deleted_at TIMESTAMPTZ DEFAULT now();
+    ADD COLUMN updated_at TIMESTAMPTZ,
+    ADD COLUMN deleted_at TIMESTAMPTZ;
 
 ALTER TABLE domain.book
     ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    ADD COLUMN updated_at TIMESTAMPTZ DEFAULT now(),
-    ADD COLUMN deleted_at TIMESTAMPTZ DEFAULT now();
+    ADD COLUMN updated_at TIMESTAMPTZ,
+    ADD COLUMN deleted_at TIMESTAMPTZ;
