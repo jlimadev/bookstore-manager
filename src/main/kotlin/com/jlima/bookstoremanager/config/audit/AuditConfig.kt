58d1f98109961d104d02552aa5c23e618a4bcf25
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-06 19:26:09 -0300

    feature(auditing): join audit classes into one single file

--- src/main/kotlin/com/jlima/bookstoremanager/config/audit/AuditConfig.kt
+++ src/main/kotlin/com/jlima/bookstoremanager/config/audit/AuditConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.Optional
 
 @Configuration
 @EnableJpaAuditing
@@ -11,5 +12,11 @@ class AuditConfig {
     @Bean
     fun auditorAware(): AuditorAware<String> {
         return CustomAuditAware()
+    }
+}
+
+class CustomAuditAware : AuditorAware<String> {
+    override fun getCurrentAuditor(): Optional<String> {
+        return Optional.of("Uzumaki Naruto").filter { it.isNotEmpty() }
     }
 }
