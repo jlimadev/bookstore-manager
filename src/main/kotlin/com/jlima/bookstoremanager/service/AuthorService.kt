5947d501f795d09219537a820fb49bc14e869eb4
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-13 08:34:25 -0300

    feature(AuthorService): implemented findById

--- src/main/kotlin/com/jlima/bookstoremanager/service/AuthorService.kt
+++ src/main/kotlin/com/jlima/bookstoremanager/service/AuthorService.kt
@@ -5,6 +5,7 @@ import com.jlima.bookstoremanager.providers.entity.domain.toDTO
 import com.jlima.bookstoremanager.providers.entity.domain.toEntity
 import com.jlima.bookstoremanager.providers.repository.domain.AuthorRepository
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class AuthorService(
@@ -15,5 +16,9 @@ class AuthorService(
         val entityToInsert = entity.toEntity()
         val createdEntity = authorRepository.save(entityToInsert)
         return createdEntity.toDTO()
+    }
+
+    override fun findById(id: UUID): AuthorDTO {
+        return authorRepository.findById(id).get().toDTO()
     }
 }
--- src/main/kotlin/com/jlima/bookstoremanager/service/CrudService.kt
+++ src/main/kotlin/com/jlima/bookstoremanager/service/CrudService.kt
@@ -1,5 +1,8 @@
 package com.jlima.bookstoremanager.service
 
+import java.util.UUID
+
 interface CrudService<T> {
     fun create(entity: T): T
+    fun findById(id: UUID): T
 }
