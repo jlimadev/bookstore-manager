3a1aaa46fdff48a448764a584a9971f5b9c8c417
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-13 18:51:03 -0300

    feature(AuthorController): create AuthorController.kt

---
+++ src/main/kotlin/com/jlima/bookstoremanager/controller/author/AuthorController.kt
@@ -0,0 +1,25 @@
+package com.jlima.bookstoremanager.controller.author
+
+import com.jlima.bookstoremanager.dto.AuthorDTO
+import com.jlima.bookstoremanager.service.AuthorService
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/v1/authors")
+class AuthorController(
+    private val authorService: AuthorService
+) : AuthorControllerDocs {
+
+    @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    override fun create(@RequestBody @Valid entity: AuthorDTO): ResponseEntity<AuthorDTO> {
+        val createdAuthor = authorService.create(entity)
+        return ResponseEntity(createdAuthor, HttpStatus.CREATED)
+    }
+}
