9bcddd05766bbf64a622a779abee80893035fc90
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-14 11:26:44 -0300

    feature(AuthorController): refactor POST test

--- src/test/kotlin/com/jlima/bookstoremanager/controller/AuthorControllerTest.kt
+++ src/test/kotlin/com/jlima/bookstoremanager/controller/AuthorControllerTest.kt
@@ -4,6 +4,8 @@ import com.jlima.bookstoremanager.controller.author.AuthorController
 import com.jlima.bookstoremanager.dto.AuthorDTO
 import com.jlima.bookstoremanager.helper.toJson
 import com.jlima.bookstoremanager.service.AuthorService
+import org.hamcrest.CoreMatchers.hasItem
+import org.hamcrest.core.Is
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.kotlin.any
@@ -48,17 +50,16 @@ class AuthorControllerTest {
 
     @Test
     fun `It should created an Author and Return 201 (Created) when POST`() {
-        // Given [Arrange]
+        // Arrange
         val (defaultDTO, entityId) = makeSut()
         val expectedResponse = defaultDTO.copy(id = entityId.toString())
 
-        // When [Act]
+        // Act
         whenever(authorService.create(any())).thenReturn(expectedResponse)
 
-        // Then [Assert]
+        // Assert
         mockMvc
             .post("/api/v1/authors") {
-                accept = MediaType.APPLICATION_JSON
                 contentType = MediaType.APPLICATION_JSON
                 content = defaultDTO.toJson()
             }.andExpect {
@@ -66,5 +67,28 @@ class AuthorControllerTest {
                 content { contentType(MediaType.APPLICATION_JSON) }
                 content { json(expectedResponse.toJson()) }
             }
+    }
+
+    @Test
+    fun `It should not create an author and return a BadRequest when POST with invalid data`() {
+        // Arrange
+        val (defaultDTO) = makeSut()
+
+        // Act
+        val invalidEntity = defaultDTO.copy(name = "")
+        val expectedContainingError = "Field: NAME: must not be empty"
+
+        // Assert
+        mockMvc.post("/api/v1/authors") {
+            contentType = MediaType.APPLICATION_JSON
+            content = invalidEntity.toJson()
+        }.andExpect {
+            status { isBadRequest() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonPath("$.statusCode", Is.`is`(400))
+            jsonPath("$.error", Is.`is`("Bad Request"))
+            jsonPath("$.message", Is.`is`("Validation error, please check the arguments."))
+            jsonPath("$.errors", hasItem(expectedContainingError))
+        }
     }
 }
