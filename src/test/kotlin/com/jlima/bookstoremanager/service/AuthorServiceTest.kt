7793b38949c0b4645f67848d9bc9d2545d4cf5ac
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-15 20:42:58 -0300

    feature(Tests): Nested tests

--- src/test/kotlin/com/jlima/bookstoremanager/service/AuthorServiceTest.kt
+++ src/test/kotlin/com/jlima/bookstoremanager/service/AuthorServiceTest.kt
@@ -7,6 +7,8 @@ import com.jlima.bookstoremanager.exception.model.BusinessEntityNotFoundExceptio
 import com.jlima.bookstoremanager.providers.entity.domain.AuthorEntity
 import com.jlima.bookstoremanager.providers.entity.domain.toEntity
 import com.jlima.bookstoremanager.providers.repository.AuthorRepository
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
@@ -22,6 +24,7 @@ import java.util.UUID
 import kotlin.test.assertEquals
 
 @ExtendWith(MockitoExtension::class)
+@DisplayName("AuthorService Test Suit")
 internal class AuthorServiceTest {
     private data class SUT(
         val sut: AuthorService,
@@ -55,82 +58,94 @@ internal class AuthorServiceTest {
         )
     }
 
-    @Test
-    fun `It should create an author correctly and return a DTO when call create with valid data`() {
-        // Arrange
-        val (sut, authorRepository, defaultDTO, defaultEntity, entityId) = makeSut()
-        val expectedCreatedEntity = defaultDTO.copy(id = entityId.toString())
+    @Nested
+    @DisplayName("Create Tests")
+    inner class Create {
+        @Test
+        fun `It should create an author correctly and return a DTO when call create with valid data`() {
+            // Arrange
+            val (sut, authorRepository, defaultDTO, defaultEntity, entityId) = makeSut()
+            val expectedCreatedEntity = defaultDTO.copy(id = entityId.toString())
 
-        // Act
-        whenever(authorRepository.save(defaultDTO.toEntity())).thenReturn(defaultEntity)
-        val response = sut.create(defaultDTO)
+            // Act
+            whenever(authorRepository.save(defaultDTO.toEntity())).thenReturn(defaultEntity)
+            val response = sut.create(defaultDTO)
 
-        // Assert
-        assertEquals(expectedCreatedEntity, response)
-        verify(authorRepository, times(1)).save(defaultDTO.toEntity())
+            // Assert
+            assertEquals(expectedCreatedEntity, response)
+            verify(authorRepository, times(1)).save(defaultDTO.toEntity())
+        }
     }
 
-    @Test
-    fun `It should return a DTO from the found entity when call findById with existing id`() {
-        // Arrange
-        val (sut, authorRepository, defaultDTO, defaultEntity, entityId) = makeSut()
-        val expectedFoundEntity = defaultDTO.copy(id = entityId.toString())
+    @Nested
+    @DisplayName("findById Tests")
+    inner class FindById {
+        @Test
+        fun `It should return a DTO from the found entity when call findById with existing id`() {
+            // Arrange
+            val (sut, authorRepository, defaultDTO, defaultEntity, entityId) = makeSut()
+            val expectedFoundEntity = defaultDTO.copy(id = entityId.toString())
 
-        // Act
-        whenever(authorRepository.findById(entityId)).thenReturn(Optional.of(defaultEntity))
-        val response = sut.findById(entityId)
+            // Act
+            whenever(authorRepository.findById(entityId)).thenReturn(Optional.of(defaultEntity))
+            val response = sut.findById(entityId)
 
-        // Assert
-        assertEquals(expectedFoundEntity, response)
-        verify(authorRepository, times(1)).findById(entityId)
-    }
-
-    @Test
-    fun `It should throw a EntityNotFoundException when call findById with non-existing id`() {
-        // Arrange
-        val (sut, authorRepository) = makeSut()
-        val randomId = UUID.randomUUID()
-        val expectedErrorMessage = "${AvailableEntities.AUTHOR} with id $randomId not found."
-
-        // Act
-        whenever(authorRepository.findById(randomId)).thenReturn(Optional.empty())
-
-        // Assert
-        val exception = assertThrows<BusinessEntityNotFoundException> {
-            sut.findById(randomId)
+            // Assert
+            assertEquals(expectedFoundEntity, response)
+            verify(authorRepository, times(1)).findById(entityId)
         }
 
-        assertEquals(expectedErrorMessage, exception.message)
-        verify(authorRepository, times(1)).findById(randomId)
+        @Test
+        fun `It should throw a EntityNotFoundException when call findById with non-existing id`() {
+            // Arrange
+            val (sut, authorRepository) = makeSut()
+            val randomId = UUID.randomUUID()
+            val expectedErrorMessage = "${AvailableEntities.AUTHOR} with id $randomId not found."
+
+            // Act
+            whenever(authorRepository.findById(randomId)).thenReturn(Optional.empty())
+
+            // Assert
+            val exception = assertThrows<BusinessEntityNotFoundException> {
+                sut.findById(randomId)
+            }
+
+            assertEquals(expectedErrorMessage, exception.message)
+            verify(authorRepository, times(1)).findById(randomId)
+        }
     }
 
-    @Test
-    fun `It should return a list of authors when findAll is called`() {
-        // Arrange
-        val (sut, authorRepository, defaultDTO, defaultEntity, entityId) = makeSut()
-        val expectedFoundEntities = listOf(defaultDTO.copy(id = entityId.toString()))
+    @Nested
+    @DisplayName("findAll Tests")
+    inner class FindAll {
+        @Test
+        fun `It should return a list of authors when findAll is called`() {
+            // Arrange
+            val (sut, authorRepository, defaultDTO, defaultEntity, entityId) = makeSut()
+            val expectedFoundEntities = listOf(defaultDTO.copy(id = entityId.toString()))
 
-        // Then
-        whenever(authorRepository.findAll()).thenReturn(listOf(defaultEntity))
-        val response = sut.findAll()
+            // Then
+            whenever(authorRepository.findAll()).thenReturn(listOf(defaultEntity))
+            val response = sut.findAll()
 
-        // Assert
-        assertEquals(expectedFoundEntities, response)
-        verify(authorRepository, times(1)).findAll()
-    }
+            // Assert
+            assertEquals(expectedFoundEntities, response)
+            verify(authorRepository, times(1)).findAll()
+        }
 
-    @Test
-    fun `It should throw BusinessEmptyResponseException when call findAll and it returns an empty response`() {
-        // Arrange
-        val (sut, authorRepository) = makeSut()
-        val expectedErrorMessage = "No ${AvailableEntities.AUTHOR}(s) found."
+        @Test
+        fun `It should throw BusinessEmptyResponseException when call findAll and it returns an empty response`() {
+            // Arrange
+            val (sut, authorRepository) = makeSut()
+            val expectedErrorMessage = "No ${AvailableEntities.AUTHOR}(s) found."
 
-        // Act
-        whenever(authorRepository.findAll()).thenThrow(BusinessEmptyResponseException(AvailableEntities.AUTHOR))
+            // Act
+            whenever(authorRepository.findAll()).thenThrow(BusinessEmptyResponseException(AvailableEntities.AUTHOR))
 
-        // Assert
-        val exception = assertThrows<BusinessEmptyResponseException> { sut.findAll() }
-        assertEquals(expectedErrorMessage, exception.message)
-        verify(authorRepository, times(1)).findAll()
+            // Assert
+            val exception = assertThrows<BusinessEmptyResponseException> { sut.findAll() }
+            assertEquals(expectedErrorMessage, exception.message)
+            verify(authorRepository, times(1)).findAll()
+        }
     }
 }
