03b70f8a712743b7aa016640bfb8e26f4b7bc0c0
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-16 10:09:49 -0300

    feature(Annotation): add annotation to ValidUUID

--- src/main/kotlin/com/jlima/bookstoremanager/dto/AuthorDTO.kt
+++ src/main/kotlin/com/jlima/bookstoremanager/dto/AuthorDTO.kt
@@ -1,11 +1,13 @@
 package com.jlima.bookstoremanager.dto
 
+import com.jlima.bookstoremanager.helper.ValidUUID
 import java.util.Date
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class AuthorDTO(
+    @field:ValidUUID
     val id: String? = null,
 
     @field:NotNull
