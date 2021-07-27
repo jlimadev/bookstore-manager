f6b3e5d1ec47489795706b116ce378609791eaa1
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-27 20:48:22 -0300

    refactor(security): create JwtAuthenticationEntrypoint class

---
+++ src/main/kotlin/com/jlima/bookstoremanager/config/security/JwtAuthenticationEntrypoint.kt
@@ -0,0 +1,18 @@
+package com.jlima.bookstoremanager.config.security
+
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class JwtAuthenticationEntrypoint : AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        authException: AuthenticationException?
+    ) {
+        response?.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+    }
+}
