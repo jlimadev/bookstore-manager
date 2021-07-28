fb538be3c1e58c7388da171d05395589551659ec
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-28 16:55:03 -0300

    feature(jwt): create JwtTokenManager to handle tokens on application

---
+++ src/main/kotlin/com/jlima/bookstoremanager/service/authentication/JwtTokenManager.kt
@@ -0,0 +1,59 @@
+package com.jlima.bookstoremanager.service.authentication
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.time.Instant
+import java.util.Date
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
+
+@Component
+class JwtTokenManager(
+    @Value("\${jwt.expiresIn}")
+    private val jwtExpiresIn: Long,
+
+    @Value("\${jwt.secret}")
+    private val jwtSecret: String,
+) {
+    fun generateToken(userDetails: UserDetails): String {
+        val claims: Map<String, JvmType.Object> = HashMap()
+        val currentTimestamp = Instant.now()
+        val issuedAt = Date.from(currentTimestamp)
+        val expirationDate = Date.from(currentTimestamp.plusSeconds(jwtExpiresIn))
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setSubject(userDetails.username)
+            .setIssuedAt(issuedAt)
+            .setExpiration(expirationDate)
+            .signWith(SignatureAlgorithm.ES512, jwtSecret)
+            .compact()
+    }
+
+    fun parseToken(token: String): Claims {
+        return Jwts.parser()
+            .setSigningKey(jwtSecret)
+            .parseClaimsJws(token)
+            .body
+    }
+
+    fun validateToken(token: String, userDetails: UserDetails): Boolean {
+        val username = getAllClaimsFromToken(token).subject
+        return username.equals(userDetails.username) && !isTokenExpired(token)
+    }
+
+    private fun getAllClaimsFromToken(token: String): Claims {
+        return Jwts.parser()
+            .setSigningKey(jwtSecret)
+            .parseClaimsJws(token)
+            .body
+    }
+
+    private fun isTokenExpired(token: String): Boolean {
+        val expirationDate = getAllClaimsFromToken(token).expiration
+        return expirationDate.before(Date.from(Instant.now()))
+    }
+}
