9d7d47709c1553d25a91d5fe178d30639301485a
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-28 18:34:26 -0300

    feature(jwt): create AuthRequest and AuthResponse

---
+++ src/main/kotlin/com/jlima/bookstoremanager/dto/response/AuthRequest.kt
@@ -0,0 +1,14 @@
+package com.jlima.bookstoremanager.dto.response
+
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
+data class AuthRequest(
+    @field:NotNull
+    @field:NotEmpty
+    val username: String,
+
+    @field:NotNull
+    @field:NotEmpty
+    val password: String
+)
---
+++ src/main/kotlin/com/jlima/bookstoremanager/dto/response/AuthResponse.kt
@@ -0,0 +1,5 @@
+package com.jlima.bookstoremanager.dto.response
+
+data class AuthResponse(
+    val accessToken: String
+)
