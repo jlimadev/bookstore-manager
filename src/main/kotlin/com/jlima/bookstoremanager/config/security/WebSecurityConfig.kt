61335ad40880c22a102dca6216e1e60def78c345
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-28 13:25:15 -0300

    feature(security): created WebSecurityConfig

---
+++ src/main/kotlin/com/jlima/bookstoremanager/config/security/WebSecurityConfig.kt
@@ -0,0 +1,85 @@
+package com.jlima.bookstoremanager.config.security
+
+import com.jlima.bookstoremanager.enums.Role
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class WebSecurityConfig(
+    private val jwtAuthenticationEntrypoint: JwtAuthenticationEntrypoint,
+    private val userDetailsService: UserDetailsService,
+    private val passwordEncoder: PasswordEncoder
+) : WebSecurityConfigurerAdapter() {
+    companion object {
+        private val ROLE_ADMIN = Role.ADMIN.name
+        private val ROLE_USER = Role.USER.name
+
+        private val AUTH_WHITELIST = arrayOf(
+            "/actuator/**",
+            "/springfox/**",
+            "/springdoc/**",
+            "/swagger-ui/",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-resources/**",
+            "/webjars/**"
+        )
+
+        private val ONLY_ADMIN = arrayOf(
+            "/authors/**",
+            "/publishers/**",
+        )
+
+        private val ALL_AUTHENTICATED = arrayOf(
+            "/books/**",
+        )
+    }
+
+    fun configureGlobal(authenticationManagerBuilder: AuthenticationManagerBuilder) {
+        authenticationManagerBuilder
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(passwordEncoder)
+    }
+
+    @Bean
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    override fun configure(httpSecurity: HttpSecurity) {
+        httpSecurity
+            .authorizeRequests()
+            .antMatchers(*AUTH_WHITELIST).permitAll()
+            .antMatchers(*ONLY_ADMIN).hasAnyRole(ROLE_ADMIN)
+            .antMatchers(*ALL_AUTHENTICATED).hasAnyRole(ROLE_ADMIN, ROLE_USER)
+            .anyRequest().authenticated()
+
+        httpSecurity
+            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntrypoint)
+
+        httpSecurity
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        httpSecurity
+            .csrf().disable()
+            .headers().frameOptions().disable()
+    }
+
+    override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers(*AUTH_WHITELIST)
+    }
+}
