47a6c2cbf197a5bf617535a140c9598f5e437ef9
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-28 18:38:26 -0300

    feature(jwt): AuthenticationService finish implementation

--- src/main/kotlin/com/jlima/bookstoremanager/service/authentication/AuthenticationService.kt
+++ src/main/kotlin/com/jlima/bookstoremanager/service/authentication/AuthenticationService.kt
@@ -1,16 +1,22 @@
 package com.jlima.bookstoremanager.service.authentication
 
 import com.jlima.bookstoremanager.dto.AuthenticatedUser
+import com.jlima.bookstoremanager.dto.response.AuthRequest
+import com.jlima.bookstoremanager.dto.response.AuthResponse
 import com.jlima.bookstoremanager.exception.model.AvailableEntities
 import com.jlima.bookstoremanager.exception.model.BusinessEntityNotFoundException
 import com.jlima.bookstoremanager.providers.repository.UserRepository
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
 class AuthenticationService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val authenticationManager: AuthenticationManager,
+    private val jwtTokenManager: JwtTokenManager
 ) : UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
         val user = userRepository.findByEmail(username)
@@ -20,5 +26,19 @@ class AuthenticationService(
             password = user.password,
             role = user.role.name
         )
+    }
+
+    fun createJwtToken(authRequest: AuthRequest): AuthResponse {
+        authenticate(authRequest.username, authRequest.password)
+        val userDetails = loadUserByUsername(authRequest.username)
+        val token = jwtTokenManager.generateToken(userDetails)
+
+        return AuthResponse(
+            accessToken = token
+        )
+    }
+
+    private fun authenticate(username: String, password: String) {
+        authenticationManager.authenticate(UsernamePasswordAuthenticationToken(username, password))
     }
 }
